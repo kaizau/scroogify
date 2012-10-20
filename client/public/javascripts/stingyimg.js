c81f4280785eb36fe1bfaf87b58d16c461e52a4e
Yifei Zhang <yz@yifei.co>
2012-10-20 13:29:05 -0400

    basic detections

--- client/public/javascripts/stingyimg.js
+++ client/public/javascripts/stingyimg.js
@@ -1,20 +1,35 @@
-; (function() {
-  var imgTags = [],
-      formattedURI = '',
-      detectPixelDensity = function() {
+var win = this.window,
+    doc = this.document,
 
-      },
-      detectViewport = function() {
+StingyImg = {
 
-      },
-      detectOrientation = function() {
+  formattedURI: '',
 
-      },
-      getImgTags = function() {
+  detectPixelRatio: function() {
+    return win.devicePixelRatio;
+  },
 
-      },
-      replaceSrc = function() {
+  detectViewportWidth: function() {
+    return doc.documentElement.clientWidth;
+  },
 
-      }
+  detectOrientation: function() {
+    var o;
+    if (win.orientation) {
+      o = (win.orientation === 0 || win.orientation === 180) ? 'portrait' : 'landscape';
+    } else {
+      var mq = win.matchMedia('orientation: portrait');
+      o = (mq.matches) ? 'portrait' : 'landscape';
+    }
+    return o;
+  },
 
-})();
+  getImgTags: function() {
+    return doc.getElementsByTagName('img');
+  },
+
+  replaceSrc: function() {
+
+  }
+
+};
